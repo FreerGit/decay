df15b324e1f27956c079605772f121f24a57f93b
FreerGit <fredrik-98@live.se>
2022-07-24 21:03:10 +0200

    feat: easier error handling

--- src/exchange/error.rs
+++ src/exchange/error.rs
@@ -1,19 +1,52 @@
-use reqwest::Error as ReqwestError;
-use serde_json::error::Error as SerdeError;
+use serde::{Deserialize, Serialize};
 use std::result;
-use std::time::SystemTimeError;
 use thiserror::Error;
 
-pub type Result<T, E = ClientError> = result::Result<T, E>;
+pub type Result<T, E = ExchangeError> = result::Result<T, E>;
 
-#[derive(Error, Debug)]
-pub enum ClientError {
-    #[error("Serde error: {0}")]
-    SerdeErr(#[from] SerdeError),
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Serialize, Deserialize)]
+pub enum ExchangeErrorType {
+    Unknown,
+    RequestError,
+    RateLimit,
+    OrderNotFound,
+    OrderCompleted,
+    InsufficientFunds,
+    InvalidOrder,
+    Authentication,
+    ParsingError,
+    ServiceUnavailable,
+}
 
-    #[error("System time error: {0}")]
-    SystemTimeErr(#[from] SystemTimeError),
+#[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize, Error)]
+#[error("Type: {error_type:?} Message: {message} Code {code:?}")]
+pub struct ExchangeError {
+    pub error_type: ExchangeErrorType,
+    pub message: String,
+    pub code: Option<i32>,
+}
 
-    #[error("Reqwest error: {0}")]
-    ReqwestError(#[from] ReqwestError),
+impl ExchangeError {
+    pub fn new(error_type: ExchangeErrorType, message: String, code: Option<i32>) -> Self {
+        Self {
+            error_type,
+            message,
+            code,
+        }
+    }
+
+    pub fn request_error(message: String, code: i32) -> Self {
+        ExchangeError::new(ExchangeErrorType::RequestError, message, Some(code))
+    }
+
+    pub fn parsing_error(message: String) -> Self {
+        ExchangeError::new(ExchangeErrorType::ParsingError, message, None)
+    }
+    pub fn unknown_error(message: &str) -> Self {
+        Self {
+            error_type: ExchangeErrorType::Unknown,
+            message: message.to_owned(),
+            code: None,
+        }
+    }
 }
